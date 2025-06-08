014ece5fd4c7ae004e8feb924ff84a44de673959
KavinduAmalka <146797841+KavinduAmalka@users.noreply.github.com>
2025-06-08 15:56:52 +0530

    create basic CRUD operations

--- AmalkaVlog-backend/controller.js
+++ AmalkaVlog-backend/controller.js
@@ -1,28 +1,54 @@
-const users =[
-  {
-    id: 1,
-    name: 'John Doe',
-  },
-  {
-    id:2,
-    name: 'Jane Smith',
-  }
-];
+const { response } = require('express');
+const User = require('./model');
 
-const getUsers = (cb) =>{
-  cb(users);
+const getUsers = (req, res, next) =>{
+  User.find()
+    .then(response =>{
+      res.status(200).json({response});
+    })
+    .catch(error =>{
+      res.status(500).json({error});
+    });
 };
 
-const getUserById = (id, cb) => {
-   const user = users.find(user => user.id == id);
-  if (!user) {
-    return cb({ message: 'User not found' });
-  }
-  else {
-    return cb(user);
-  }
-   
+const addUser = (req, res, next) => {
+  const user = new User({
+    id: req.body.id,
+    name: req.body.name
+  });
+  user.save()
+    .then(response => {
+      res.status(201).json({response});
+    })
+    .catch(error => {
+      res.status(500).json({error});
+    });
+
+};
+
+const updateUser = (req,res,next) =>{
+  const { id, name } = req.body;
+  User.updateOne({ id:id }, {$set:{name:name}})
+    .then(response => {
+        res.status(201).json({response});
+      })
+      .catch(error => {
+        res.status(500).json({error});
+      });
+};
+
+const deleteUser = (req,res,next) =>{
+  const id = req.body.id;
+  User.deleteOne({id:id})
+    .then(response => {
+          res.status(201).json({response});
+        })
+        .catch(error => {
+          res.status(500).json({error});
+        });
 };
 
 exports.getUsers = getUsers;
-exports.getUserById = getUserById;
+exports.addUser = addUser;
+exports.updateUser = updateUser;
+exports.deleteUser = deleteUser;
